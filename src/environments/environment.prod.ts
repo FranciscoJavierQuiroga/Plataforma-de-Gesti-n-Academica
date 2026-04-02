72dbe9c3ea189b317f9326aa22d41a2e5166d0a4
Francisco Quiroga <francisco.quiroga@uptc.edu.co>
2026-04-02 14:09:35 -0500

    Fix Lambda API URLs: remove trailing slashes to prevent double slash errors

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   api: {
-    login: 'https://fruxf8d3n1.execute-api.us-east-1.amazonaws.com/dev/',
-    students: 'https://5jkuxj0bab.execute-api.us-east-1.amazonaws.com/dev/',
-    teachers: 'https://99mx4xakrd.execute-api.us-east-1.amazonaws.com/dev/',
-    admin: 'https://y473tgr8m1.execute-api.us-east-1.amazonaws.com/dev/',
-    administrator: 'https://y473tgr8m1.execute-api.us-east-1.amazonaws.com/dev/',
-    groups: 'https://wyotbd7a1l.execute-api.us-east-1.amazonaws.com/dev/'
+    login: 'https://fruxf8d3n1.execute-api.us-east-1.amazonaws.com/dev',
+    students: 'https://5jkuxj0bab.execute-api.us-east-1.amazonaws.com/dev',
+    teachers: 'https://99mx4xakrd.execute-api.us-east-1.amazonaws.com/dev',
+    admin: 'https://y473tgr8m1.execute-api.us-east-1.amazonaws.com/dev',
+    administrator: 'https://y473tgr8m1.execute-api.us-east-1.amazonaws.com/dev',
+    groups: 'https://wyotbd7a1l.execute-api.us-east-1.amazonaws.com/dev'
   }
 };
